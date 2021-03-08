f293f46ca272bfaf82bc5e749d31dc3de7b0ebc6
James Carlson <carlso70@purdue.edu>
2021-03-07 22:09:27 -0600

    add links to details

--- scrape.py
+++ scrape.py
@@ -1,26 +1,21 @@
-from typing import Tuple, cast
-import requests
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
-from bs4 import BeautifulSoup
-import re
-import pandas as pd
+import json
 import os
+import re
+from typing import Tuple, cast
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import (NoSuchElementException,
+                                        StaleElementReferenceException)
+from selenium.webdriver.common.keys import Keys
 
 # We use selenium to automate some web browser stuff that beautiful soup can't.
 # That means we can click the javascript links on the website (ex: click accept terms and agreement stuff)
 
 
-# DOWNLOAD THIS THING HERE https://github.com/mozilla/geckodriver/releases and save it on your comp
-
-
-def element_exists_by_tag_name(driver, element) -> bool:
-    try:
-        driver.find_element_by_tag_name(element)
-    except:
-        return False
-    return True
+# TO GET GECKODRIVER.EXE yourself DOWNLOAD THIS THING HERE https://github.com/mozilla/geckodriver/releases and save it on your comp
 
 
 # returns a tuple (x,y,z). Displaying x to y of z results
@@ -33,74 +28,58 @@ def parse_result_count(display_results_string: str) -> tuple:
 
 
 def accept_terms(driver) -> None:
-    accept_button = driver.find_element_by_id(
-        'ctl00_mainContentArea_disclaimerContent_yesButton')
-    print("accept-button =>", accept_button)
+    accept_button = driver.find_element_by_id('ctl00_mainContentArea_disclaimerContent_yesButton')
     accept_button.click()
 
 
-def get_links_to_issuer_homepage_details(driver) -> list:
+def get_links_in_table(driver, table_id) -> list:
     # get the details, this code pretty much the same as the issuers
+    links = []
+    while True:
+        # find table on page and add links to each issuer
+        for cell in driver.find_elements_by_css_selector('td'):
+            try:
+                link = cell.find_element_by_tag_name('a').get_attribute('href')
+                links.append(link)
+            except NoSuchElementException:
+                pass
 
+        # find 'next' button to get the next list
+        try:
+            next_button = driver.find_element_by_class_name('next')
+        except NoSuchElementException:
+            print("no items on this page: ", driver.current_url)
+            return links
+
+        # Check if we can go to the next page or not
+        attributes = next_button.get_attribute('class').split(" ")
+        for attr in attributes:
+            if attr == "disabled":
+                return links
+
+        # go to next page
+        next_button.click()
+
+
+def scrape_for_links_to_details(driver, links_to_issuers) -> list:
     links_to_details = []
-    while True:
-        # find table on page and add links to each issuer
-        table = driver.find_element_by_id('lvIssues')
-        for row in table.find_elements_by_css_selector('tr'):
-            for cell in row.find_elements_by_css_selector('td'):
-                try:
-                    link = cell.find_element_by_tag_name(
-                        'a').get_attribute('href')
-                    links_to_details.append(link)
-                except NoSuchElementException:
-                    pass
-
-        # find next button
-        next_button = driver.find_element_by_class_name('next')
-
-        # Check if we can go to the next page or not
-        attributes = next_button.get_attribute('class').split(" ")
-        for attr in attributes:
-            if attr == "disabled":
-                return links_to_details
-        # go to next page
-        next_button.click()
+    for index, link in enumerate(links_to_issuers):
+        print("getting details on link", index, "of", len(links_to_issuers))
+        driver.get(link)
+        links_to_details.extend(get_links_in_table(driver, ISSUER_PAGE_DETAIL_LINK_TABLE_ID))
+        print("current detail count", len(links_to_details))
+    return links_to_details
 
 
-def get_links_to_issuers(driver) -> list:
-    # Get all the links to each issuer from the main page
+if __name__ == "__main__":
+    MAIN_PAGE_ISSUER_LINK_TABLE_ID = 'lvIssuers'
+    ISSUER_PAGE_DETAIL_LINK_TABLE_ID = 'lvIssues'
 
-    links_to_issuers = []
-    while True:
-        # find table on page and add links to each issuer
-        table = driver.find_element_by_id('lvIssuers')
-        for row in table.find_elements_by_css_selector('tr'):
-            for cell in row.find_elements_by_class_name('sorting_1'):
-                # this is the blue link part of each table row
-                try:
-                    link = cell.find_element_by_tag_name(
-                        'a').get_attribute('href')
-                    links_to_issuers.append(link)
-                except NoSuchElementException:
-                    pass
+    LINKS_TO_ISSUERS_FILE = "links_to_issuers.json"
+    LINKS_TO_ISSUERS_DETAILS_FILE = "links_to_issuers_details.json"
 
-        print("link to issuer array size", len(links_to_issuers))
-        # find next button
-        next_button = driver.find_element_by_class_name('next')
-
-        # Check if we can go to the next page or not
-        attributes = next_button.get_attribute('class').split(" ")
-        for attr in attributes:
-            if attr == "disabled":
-                return links_to_issuers
-        # go to next page
-        next_button.click()
-
-
-def main():
     driver = webdriver.Firefox(executable_path='.\geckodriver.exe')
     driver.maximize_window()  # maximize so all elements are clickable
-    driver.implicitly_wait(0.5)
 
     # Get website loaded
     driver.get("https://emma.msrb.org/IssuerHomePage/State?state=IL")
@@ -108,17 +87,33 @@ def main():
     # use selenium to accept terms and agreement
     accept_terms(driver)
 
-    links_to_issuers = get_links_to_issuers(driver)
-    print("issuers", links_to_issuers)
+    # gets links_to_issuers from file from previous run or scrape the site
+    links_to_issuers = []
+    try:
+        with open(LINKS_TO_ISSUERS_FILE) as json_file:
+            links_to_issuers = json.load(json_file)
+    except FileNotFoundError:
+        print(f"no {LINKS_TO_ISSUERS_FILE}.....scraping website for new data")
+        links_to_issuers = get_links_in_table(driver, MAIN_PAGE_ISSUER_LINK_TABLE_ID)
 
-    # Go to each issuer's securities table    
+        # Save issuers links
+        with open(LINKS_TO_ISSUERS_FILE, 'w') as issuer_link_file:
+            json.dump(links_to_issuers, issuer_link_file, indent=4)
+
+    driver.implicitly_wait(0.1)  # Implicit wait is needed for detail page
+
+    # Go to each issuer's securities table
     links_to_details = []
-    for l in links_to_issuers:
-        driver.get(l)
-        detail_links = get_links_to_issuer_homepage_details(driver)
-        links_to_details.extend(detail_links)
-    
-    print("details", links_to_details)
+    try:
+        with open(LINKS_TO_ISSUERS_DETAILS_FILE) as json_file:
+            links_to_details = json.load(json_file)
+    except FileNotFoundError:
+        print(f"no {LINKS_TO_ISSUERS_DETAILS_FILE}...scraping website for new data")
+        links_to_details = scrape_for_links_to_details(driver, links_to_issuers)
+        # Save details
+        with open(LINKS_TO_ISSUERS_DETAILS_FILE, 'w') as details_links_file:
+            json.dump(links_to_details, details_links_file, indent=4)
 
-if __name__ == "__main__":
-    main()
+    print(links_to_details)
+    
+
